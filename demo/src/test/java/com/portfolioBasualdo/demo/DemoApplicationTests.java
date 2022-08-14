8093c5c536f0b5a0958201a43026100f40bab42a
Jose <jose_esteban_basualdo@hotmail.com>
2022-08-14 00:31:37 -0300

    adds project from initializr

---
+++ demo/src/test/java/com/portfolioBasualdo/demo/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.portfolioBasualdo.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
