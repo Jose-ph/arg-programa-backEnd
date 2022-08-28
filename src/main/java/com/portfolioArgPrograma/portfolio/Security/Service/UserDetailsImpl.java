9c413bbc9d53febb1a68fbfe816e4dc0248eebfb
Jose-ph <68978572+Jose-ph@users.noreply.github.com>
2022-08-27 23:34:40 -0300

    Update UserDetailsImpl.java
    missing @service notation made build failure

--- src/main/java/com/portfolioArgPrograma/portfolio/Security/Service/UserDetailsImpl.java
+++ src/main/java/com/portfolioArgPrograma/portfolio/Security/Service/UserDetailsImpl.java
@@ -10,11 +10,14 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
 
 /**
  *
  * @author jose_
  */
+
+@Service
 public class UserDetailsImpl  implements UserDetailsService {
     
      @Autowired
