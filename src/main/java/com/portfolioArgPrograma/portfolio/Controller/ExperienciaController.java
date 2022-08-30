628386a11db41da1a1195a7ebd73cad084e05ac5
Jose-ph <68978572+Jose-ph@users.noreply.github.com>
2022-08-29 23:18:55 -0300

    delete PreAuthorize

--- src/main/java/com/portfolioArgPrograma/portfolio/Controller/ExperienciaController.java
+++ src/main/java/com/portfolioArgPrograma/portfolio/Controller/ExperienciaController.java
@@ -13,7 +13,7 @@ import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.access.prepost.PreAuthorize;
+//import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -40,7 +40,7 @@ public class ExperienciaController {
         return new ResponseEntity(list,HttpStatus.OK);
     
     }
-    @PreAuthorize("hasRole('ADMIN')")
+ 
     @PostMapping("/create")
    public ResponseEntity <?> create(@RequestBody DtoExperiencia dtoExperiencia){
        
@@ -62,7 +62,7 @@ public class ExperienciaController {
         return new ResponseEntity(experiencia, HttpStatus.OK);
     }
    
-    @PreAuthorize("hasRole('ADMIN')")
+ 
     @PutMapping("/update/{id}")
     public ResponseEntity<?> update(@PathVariable("id") int id, @RequestBody DtoExperiencia dtoExperiencia ){
     
@@ -86,7 +86,7 @@ public class ExperienciaController {
         
     }
     
-    @PreAuthorize("hasRole('ADMIN')")
+ 
     @DeleteMapping("/delete/{id}")
     public ResponseEntity <?> delete(@PathVariable("id") int id){
     
